8cda12736de221733dd08ecf330af24256242e2a
dimitry <dimabymerang0@gmail.com>
2021-04-22 22:24:32 +0300

    Changed allowed origins

--- src/main/java/com/saturn/configuration/WebSocketConfig.java
+++ src/main/java/com/saturn/configuration/WebSocketConfig.java
@@ -28,7 +28,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000")
+                .setAllowedOrigins("http://89.108.65.167:3000")
                 .withSockJS();
     }
 
